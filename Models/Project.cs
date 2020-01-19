0114916adb69d776b26d682e80f31bde37a64161
Mauro Lavigna <mauro.lavigna@gmail.com>
2020-01-19 15:20:32 -0300

    Display projects in cards instead of a table

---
+++ Models/Project.cs
@@ -0,0 +1,8 @@
+public class Project
+{
+    public string Name { get; set; }
+    public string Date { get; set; }
+    public string Technology { get; set; }
+    public string Summary { get; set; }
+    public string[] Contributions { get; set; }
+}

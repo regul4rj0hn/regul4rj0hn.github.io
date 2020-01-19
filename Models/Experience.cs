1a100e0444b388935d0448e54dfc5b6ff37d96b9
Mauro Lavigna <mauro.lavigna@gmail.com>
2020-01-19 16:23:27 -0300

    Update Experience section

---
+++ Models/Experience.cs
@@ -0,0 +1,11 @@
+namespace resume.Models
+{
+    public class Experience
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Date { get; set; }
+        public string Summary { get; set; }
+    }
+}
+
